2dcb5f114d1a750c1f0d5c2f13e142c64f412fa0
manishtailor106 <85630394+manishtailor106@users.noreply.github.com>
2024-10-16 20:16:05 +0530

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-import numpy as np
 from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
+    title = "my Flask App"
+    return render_template("index.html",title=title)
 
-    return render_template("index.html")
-
-
-
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
