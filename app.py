85aedc524eb593d76460dad9a8c1cf865768ccba
manishtailor106 <85630394+manishtailor106@users.noreply.github.com>
2024-10-16 20:16:50 +0530

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     title = "my Flask App"
-    return render_template("index.html",title=title)
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
